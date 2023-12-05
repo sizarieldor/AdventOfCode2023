d99551bbea336cf677556b682a53553081de7055
Ivan Toshev <ivan.toshev.ext@ewe.de>
2023-12-05 12:53:58 +0200

    1

--- src/main/java/org/example/Day1.java
+++ src/main/java/org/example/Day1.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Day1 {
 
@@ -8,15 +10,92 @@ public class Day1 {
         BufferedReader br = new BufferedReader(new FileReader(input));
         String line;
         int result = 0;
+        int inputLineCounter = 0;
         while ((line = br.readLine()) != null) {
-            int lineVal = getLineSum(line);
+            inputLineCounter++;
+            String lineDigits = replaceStrDigits(line, inputLineCounter);
+            int lineVal = getLineSum(lineDigits);
             result += lineVal;
         }
+
         br.close();
         System.out.println(result);
     }
 
+    private static String replaceStrDigits(String line, int inputLineCounter) {
+        String firstStr = "";
+        Map<String, String> dict = new HashMap<>();
+
+        try {
+            dict.put("one", "1");
+            dict.put("two", "2");
+            dict.put("three", "3");
+            dict.put("four", "4");
+            dict.put("five", "5");
+            dict.put("six", "6");
+            dict.put("seven", "7");
+            dict.put("eight", "8");
+            dict.put("nine", "9");
+
+            //replace first instance of substring
+            int startIndex = Integer.MAX_VALUE;
+
+            for (String key : dict.keySet()) {
+                int keyPos = line.indexOf(key);
+                if (keyPos != -1 && keyPos < startIndex) {
+                    startIndex = keyPos;
+                    firstStr = key;
+                }
+            }
+            if ("" != firstStr) {
+                line = line.replaceFirst(firstStr, dict.get(firstStr));
+            }
+
+            //replace last substring
+            startIndex = Integer.MAX_VALUE;
+            String lastStrRev = "";
+            //reverse the input line
+            String lineRev = reverseStr(line);
+            String keyRev = "";
+            for (String key : dict.keySet()) {
+                //reverse the key
+                keyRev = reverseStr(key);
+
+                int keyRevPos = lineRev.indexOf(keyRev);
+                if (keyRevPos != -1 && keyRevPos < startIndex) {
+                    startIndex = keyRevPos;
+                    lastStrRev = keyRev;
+                }
+            } //the string value of the last number has been found
+
+            //find the digit value of the last number
+            if ("" != lastStrRev) {
+                String valueOfRev = reverseStr(lastStrRev);
+                lineRev = lineRev.replaceFirst(lastStrRev, dict.get(valueOfRev));
+            }
+
+
+            line = reverseStr(lineRev);
+        } catch (NullPointerException ne) {
+            System.out.println("firstStr is " + firstStr + " on line " + inputLineCounter);
+        } finally {
+            return line;
+        }
+
+    }
+
     private static int getLineSum(String line) {
+        Map<String, String> dict = new HashMap<>();
+        dict.put("one", "1");
+        dict.put("two", "2");
+        dict.put("three", "3");
+        dict.put("four", "4");
+        dict.put("five", "5");
+        dict.put("six", "6");
+        dict.put("seven", "7");
+        dict.put("eight", "8");
+        dict.put("nine", "9");
+
         int lineSum = 0;
         char[] lineChars = line.toCharArray();
 
@@ -40,14 +119,12 @@ public class Day1 {
             }
         }
 
-        lineSum = 10*firstC + lastC;
-/*        for (int i = 0; i < line.length(); i++) {
-            if(Character.isDigit(line.charAt(i))){
-                int cVal = (int)line.charAt(i);
-                lineSum += cVal;
-            }
-        }*/
-
+        lineSum = 10 * Character.getNumericValue(firstC) + Character.getNumericValue(lastC);
         return lineSum;
+    }
+
+    private static String reverseStr(String input) {
+        StringBuilder sb = new StringBuilder(input);
+        return sb.reverse().toString();
     }
 }
