12147dd2e31852add1cbf0341592dff418a70a9d
Ivan Toshev <ivan.toshev.ext@ewe.de>
2024-01-01 18:18:37 +0200

    working solution

--- src/main/java/org/example/Day2.java
+++ src/main/java/org/example/Day2.java
@@ -1,7 +1,10 @@
 package org.example;
 
 import java.io.*;
-
+// line = "Game1:13green,3red;4red,9green,4blue;9green,10red,2blue"
+// lineData = "13green,3red;4red,9green,4blue;9green,10red,2blue"
+// arrAllDrawsInLine = {"13green,3red" , "4red,9green,4blue" , "9green,10red,2blue"}
+// singleDraw = "13green,3red"
 
 public class Day2 {
     public static void solveDay2(File input) throws IOException {
@@ -9,27 +12,54 @@ public class Day2 {
         String line = "";
         int lineNum = 1;
         int totalSum = 0;
+        int totalPower = 0;
+
         while ((line = br.readLine()) != null) {
             line = line.replaceAll(" ", "");
             String[] arrLineSplit = line.split(":");
             String lineData = arrLineSplit[1];
             String[] arrAllDrawsInLine = lineData.split(";");
-            boolean allDrawsAreValid = true;
 
+            boolean allDrawsAreValid = true;
             for (int i = 0; i < arrAllDrawsInLine.length; i++) {
                 String singleDraw = arrAllDrawsInLine[i];
                 if (!checkIfDrawValid(singleDraw)) {
                     allDrawsAreValid = false;
                 }
             }
-
             if (allDrawsAreValid) {
                 totalSum += lineNum;
             }
-
             lineNum++;
-        }
+
+// line = "Game1:13green,3red;4red,9green,4blue;9green,10red,2blue"
+// lineData = "13green,3red;4red,9green,4blue;9green,10red,2blue"
+// arrAllDrawsInLine = {"13green,3red" , "4red,9green,4blue" , "9green,10red,2blue"}
+// singleDraw = "13green,3red"
+
+            //Part 2 logic
+            int linePower = 0;
+            int minRedNeed = Integer.MIN_VALUE;
+            int minGreenNeed = Integer.MIN_VALUE;
+            int minBlueNeed = Integer.MIN_VALUE;
+            int[] maxColors = new int[3];
+            maxColors[0] = minRedNeed;
+            maxColors[1] = minGreenNeed;
+            maxColors[2] = minBlueNeed;
+
+            for (int i = 0; i < arrAllDrawsInLine.length; i++) {
+                String singleDraw = arrAllDrawsInLine[i];
+                maxColors = findMinRequired(singleDraw, maxColors);
+
+            }
+
+            int power = maxColors[0] * maxColors[1] * maxColors[2];
+            totalPower += power;
+
+        } //go to next line
+
         System.out.println(totalSum);
+        System.out.println(totalPower);
     }
 
     /*
@@ -38,6 +68,44 @@ public class Day2 {
     Game 3: 1 red, 9 green, 3 blue; 8 green, 4 red, 11 blue; 6 red, 10 blue; 6 green, 6 red, 12 blue; 2 blue, 11 green, 7 red; 12 blue, 9 green, 8 red
     Game 4: 7 red, 2 green, 1 blue; 12 green; 12 green
      */
+
+// line = "Game1:13green,3red;4red,9green,4blue;9green,10red,2blue"
+// lineData = "13green,3red;4red,9green,4blue;9green,10red,2blue"
+// arrAllDrawsInLine = {"13green,3red" , "4red,9green,4blue" , "9green,10red,2blue"}
+// singleDraw = "13green,3red"
+
+    private static int[] findMinRequired(String singleDraw, int[] singleDrawMaxes) {
+        int maxRed = singleDrawMaxes[0];
+        int maxGreen = singleDrawMaxes[1];
+        int maxBlue = singleDrawMaxes[2];
+
+        String[] tokens = singleDraw.split(",");
+        for (int i = 0; i < tokens.length; i++) {
+            String[] splitToken = splitToken(tokens[i]);
+            int cubeCount = Integer.parseInt(splitToken[0]);
+            String cubeColor = splitToken[1];
+
+            if ("red".equals(cubeColor)) {
+                if (cubeCount > maxRed) {
+                    maxRed = cubeCount;
+                }
+            } else if ("green".equals(cubeColor)) {
+                if (cubeCount > maxGreen) {
+                    maxGreen = cubeCount;
+                }
+            } else if ("blue".equals(cubeColor)) {
+                if (cubeCount > maxBlue) {
+                    maxBlue = cubeCount;
+                }
+            }
+        }
+
+        singleDrawMaxes[0] = maxRed;
+        singleDrawMaxes[1] = maxGreen;
+        singleDrawMaxes[2] = maxBlue;
+
+        return singleDrawMaxes;
+    }
 
     private static boolean checkIfDrawValid(String singleDraw) {
         //only 12 red cubes, 13 green cubes, and 14 blue cubes
@@ -48,7 +116,6 @@ public class Day2 {
 
         String[] tokens = singleDraw.split(",");
         for (int i = 0; i < tokens.length; i++) {
-
             String[] splitToken = splitToken(tokens[i]);
             int cubeCount = Integer.parseInt(splitToken[0]);
             String cubeColor = splitToken[1];
